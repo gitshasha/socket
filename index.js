71f64f3f7d38e0c85ddab1d8cdd5fbe9b37a013c
shashank <shashankkyadari172@gmail.com>
2022-06-02 22:51:54 +0530

    socket dep

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const io = require("socket.io")(8900, {
+const PORT = process.env.PORT;
+
+const io = require("socket.io")(PORT || 8900, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://findeve.herokuapp.com",
   },
 });
 
